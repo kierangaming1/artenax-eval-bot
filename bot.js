f3d1740b64aee5ef6f716e0a78afeb28a488e0d1
Josh <19347018+DefinitelyNotJosh@users.noreply.github.com>
2018-03-25 17:29:24 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,6 +11,24 @@ client.on('message', message => {
   	}
 });
 
+client.on("message", message => {
+  const args = message.content.split(" ").slice(1);
+
+  if (message.content.startsWith("eval");
+    try {
+      const code = args.join(" ");
+      let evaled = eval(code);
+
+      if (typeof evaled !== "string")
+        evaled = require("util").inspect(evaled);
+
+      message.channel.send(clean(evaled), {code:"xl"});
+    } catch (err) {
+      message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+    }
+  }
+});
+
 client.on('message', message => {
     if (message.content === 'bing') {
     	message.reply('BONG!');
